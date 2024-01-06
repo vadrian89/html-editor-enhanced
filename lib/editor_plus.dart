3faf5e41a5fa70224bbbfeee9c6a5f6ee400c4b8
Adrian Verban <verbanady@gmail.com>
2024-01-06 09:36:53 +0200

    Refactored plus libraries

--- lib/editor_plus.dart
+++ lib/editor_plus.dart
@@ -1,10 +1,6 @@
-library html_editor_plus;
-
-export 'core.dart';
-
-export 'editor_field.dart';
+library editor_plus;
 
 export 'src/plus/editor/html_editor_unsupported.dart'
     if (dart.library.html) 'src/plus/editor/html_editor_web.dart'
-    if (dart.library.io) 'src/plus/editor/html_editor_mobile.dart';
+    if (dart.library.io) 'src/plus/editor/html_editor_mobile.dart' show HtmlEditor;
 export 'src/plus/editor_controller.dart' show HtmlEditorController;
